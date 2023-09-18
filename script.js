f79b8def9061cdfa2122b0170719898991f1ff98
minacrefetejoaozinho2000 <140417709+minacrefetejoaozinho2000@users.noreply.github.com>
2023-09-18 11:52:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+botao.addEventListener('click', function (){
+
+
+var nomeCliente = nome.value;
+var quantiaSorvete = quantidade.value;
+var saborSorvete = sabores.value
+
+
+
+
+
+//alert("nome: " + nomeCliente + " qnt: " + quantiasorvete + "sabor: " + saborsorvete);
+
+
+pedidonome.innerHTML = nomeCliente;
+pedidoquantia.innerHTML = quantia
+
+
+var dado pedido
+});
